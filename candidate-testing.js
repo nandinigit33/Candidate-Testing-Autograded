1d0681ee98a043112509899ca8697dbdf82fb40e
nandinigit33 <nandinids33@gmail.com>
2024-06-27 23:58:00 -0400

    committing candidate-testing.js task 3

--- candidate-testing.js
+++ candidate-testing.js
@@ -8,12 +8,17 @@ let candidateName = "";
 let question = "Who was the first American woman in space? ";
 let correctAnswer = "Sally Ride";
 let candidateAnswer = "";
+let numberOfQuizQuestions = 5;
 
 
 //TODO: Variables for Part 2
-let questions;
-let correctAnswers;
-let candidateAnswers;
+let questions = ["Who was the first American woman in space? ", 
+"True or false: 5 kilometer == 5000 meters? ", 
+"(5 + 3)/2 * 10 = ? ",
+"Given the array [8, 'Orbit', 'Trajectory', 45], what entry is at index 2? ",
+"What is the minimum crew size for the ISS? "];
+let correctAnswers = ["Sally Ride", "true", "40", "Trajectory", "3"];
+let candidateAnswers=[];
 
 
 function askForName() {
@@ -24,21 +29,37 @@ function askForName() {
 
 function askQuestion() {
   // TODO 1.2b: Ask candidate the question and assign the response as candidateAnswer //
-console.log(question);
-candidateAnswer = input.question("Please enter your answer for question 1 : ")
 
+for (let i = 0; i < questions.length; i++){
+  let answerFromCandidate = input.question(questions[i]);
+   candidateAnswers.push(answerFromCandidate);
+   console.log("Your Answer : " +candidateAnswers[i]); 
+   console.log("Correct Answer : " +correctAnswers[i]); 
+  }
 }
 
-function gradeQuiz(candidateAnswers) {
 
-  // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
-console.log("Your answer for question 1 is correct.")
+function gradeQuiz(candidateAnswers) {
+   // TODO 1.2c: Let the candidate know if they have answered the question correctly or incorrectly // 
+   let numberOfCorrectAnswers = 0;
+   for (let i = 0; i < candidateAnswers.length; i++){
+    if(candidateAnswers[i].toLowerCase() === correctAnswers[i].toLowerCase()){
+      numberOfCorrectAnswers++;
+    }
+}
+ 
 
 
   let grade;  //TODO 3.2 use this variable to calculate the candidates score.
-
-
-  return grade;
+grade=((numberOfCorrectAnswers/numberOfQuizQuestions)*100);
+if(grade >=80){
+  console.log(">>> Overall Grade: " +grade+ "% ( " +numberOfCorrectAnswers+ " of " +numberOfQuizQuestions+ " responses correct) <<<");
+  console.log("Status Passed")
+}else{
+console.log(">>> Overall Grade: " +grade+ "% (" +numberOfCorrectAnswers+ " of "+numberOfQuizQuestions+ " responses correct) <<<");
+  console.log("Status Failed")
+}
+return grade;
 }
 
 function runProgram() {
@@ -47,6 +68,7 @@ function runProgram() {
    console.log("Hi, Welcome for the Candidate Testing Quiz " +candidateName);
   askQuestion();
   gradeQuiz(this.candidateAnswers);
+  
 }
 
 // ----------- Don't write any code or change any code below this line ---------- //
@@ -61,28 +83,3 @@ module.exports = {
   gradeQuiz: gradeQuiz,
   runProgram: runProgram
 };
-/*
-Candidate Name: Can Twin
-1) Who was the first American woman in space?
-Your Answer: sally ride
-Correct Answer: Sally Ride
-
-2) True or false: 5000 meters = 5 kilometers.
-Your Answer: false
-Correct Answer: true
-
-3) (5 + 3)/2 * 10 = ?
-Your Answer: 45
-Correct Answer: 40
-
-4) Given the array [8, "Orbit", "Trajectory", 45], what entry is at index 2?
-Your Answer: trajectory
-Correct Answer: Trajectory
-
-5) What is the minimum crew size for the ISS?
-Your Answer: 10
-Correct Answer: 3
-
->>> Overall Grade: 40% (2 of 5 responses correct) <<<
->>> Status: FAILED <<<
-*/
